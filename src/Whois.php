646415da6edfbb61928f35b9a32d29e19505eda2
William Knauss <william.knauss@kingdom.com>
2023-02-16 17:01:50 -0500

    question 2

--- src/Whois.php
+++ src/Whois.php
@@ -1,23 +1,42 @@
 <?php
 
+/**
+ * Class for querying a WHOIS server.
+ */
 class Whois {
-  private $server;
-
-  public function __construct($server) {
-    $this->server = $server;
-  }
-
-  public function query($domain) {
-    $socket = fsockopen($this->server, 43);
-    if (!$socket) {
-      return false;
+    /**
+     * The hostname of the WHOIS server to query.
+     *
+     * @var string
+     */
+    private $server;
+  
+    /**
+     * Constructor for the Whois class.
+     *
+     * @param string $server The hostname of the WHOIS server to query.
+     */
+    public function __construct($server) {
+      $this->server = $server;
     }
-    fwrite($socket, "$domain\r\n");
-    $response = '';
-    while (!feof($socket)) {
-      $response .= fgets($socket, 128);
+  
+    /**
+     * Queries the WHOIS server for the specified domain name.
+     *
+     * @param string $domain The domain name to query.
+     * @return string|false The response from the WHOIS server, or false on failure.
+     */
+    public function query($domain) {
+      $socket = fsockopen($this->server, 43);
+      if (!$socket) {
+        return false;
+      }
+      fwrite($socket, "$domain\r\n");
+      $response = '';
+      while (!feof($socket)) {
+        $response .= fgets($socket, 128);
+      }
+      fclose($socket);
+      return $response;
     }
-    fclose($socket);
-    return $response;
   }
-}
